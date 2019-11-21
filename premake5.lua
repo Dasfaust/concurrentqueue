2905dcf6afb4487b6ff8b48705f8df4904654434
Cody Caudill <ccaudill23@gmail.com>
2019-11-21 17:11:16 -0500

    Update premake

--- premake5.lua
+++ premake5.lua
@@ -1,24 +1,20 @@
 project "concurrentqueue"
     kind "StaticLib"
     language "C++"
+    systemversion "latest"
+    cppdialect "C++17"
+    staticruntime "on"
     
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    objdir ("build/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
         "concurrentqueue.h"
     }
     
-	filter "system:windows"
-        systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
-       
-  filter "system:linux"
-        systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
-        
-  filter { "system:windows", "configurations:Release" }
-      buildoptions "/MT"
+    filter "configurations:release"
+        optimize "on"
+
+    filter "configurations:dist"
+        optimize "on"
